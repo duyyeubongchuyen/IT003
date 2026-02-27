098e2bd296551d3e4f298c2a7bb83a34f0861fb3
duyyeubongchuyen <nguyenthaiduy0712@gmail.com>
2026-02-27 12:48:37 +0700

    feat : implement insertion sorting algorithm

--- Sorting/SelectionInsertionSort.cpp
+++ Sorting/SelectionInsertionSort.cpp
@@ -21,10 +21,27 @@ void selectionSort(int a[], int size) {// Duyệt min với current
 	}
 }
 
-void insert(int a[], int size, int curindex,int newindex){
-	for (int i = newindex; i < size-1; i++) {
-		int temp = a[i];
-		a[i + 1] = temp;
+void insertionSort(int a[], int size) {
+	for (int i = 1; i < size; i++) {
+		int j = i - 1;
+		int temp = i;
+		while (j >= 0 && (a[temp] < a[j])) {
+			swapArray(a, size, temp, j);
+			j--;
+			temp--;
+		}
+	}
+}
+
+void insertionSortLegit(int a[], int size) {
+	for (int i = 0; i < size; i++) {
+		int j = i - 1;
+		int key = a[i];
+		while (j >= 0 && (a[j] > key)) {
+			a[j + 1] = a[j];
+			j--;
+		}
+		a[j + 1] = key;
 	}
 }
 
@@ -33,10 +50,12 @@ void printArray(int a[], int size) {
 }
 
 int main() {
-	int a[] = { 9,0,1,5,3,2 };
+	int a[] = { 9,10,0,0,3,2 };
 	printArray(a, 6);
 	cout << endl;
-	selectionSort(a, 6);
+	/*selectionSort(a, 6);
+	printArray(a, 6);*/
+	insertionSortLegit(a, 6);
 	printArray(a, 6);
 }
 
