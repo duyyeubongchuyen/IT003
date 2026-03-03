9f3681f018023f96c87b623407f79fddaa73a49a
duyyeubongchuyen <nguyenthaiduy0712@gmail.com>
2026-03-03 23:01:21 +0700

    feat: add merge sort algorithm

--- Sorting/CountingRadixSort/CountingRadixSort/CountingRadixSort.cpp
+++ Sorting/CountingRadixSort/CountingRadixSort/CountingRadixSort.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -51,11 +52,63 @@ void radixSort(int a[], int size) {
 		countSort(a, size, exp);
 }
 
+void merge(vector<int>& arr, int left,
+    int mid, int right) {
+
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    vector<int> L(n1), R(n2);
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0;
+    int k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(vector<int>& arr, int left, int right) {
+
+    if (left >= right)
+        return;
+
+    int mid = left + (right - left) / 2;
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+    merge(arr, left, mid, right);
+}
+
+
 int main() {
 	int a[7] = { 2,1,0,9,10,0,2 };
 	printArray(a, 7);
 	cout << endl;
-	/*countingSort(a, 7);*/
 	radixSort(a, 7);
 	printArray(a, 7);
 	return 0;
