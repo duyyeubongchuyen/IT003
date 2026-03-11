40f951115565e4527631ba238ccf4203adfc6ba0
duyyeubongchuyen <nguyenthaiduy0712@gmail.com>
2026-03-11 23:28:13 +0700

    dip: implementing merge sort and heap sort algorithm

--- Sorting/HeapQuickSort/HeapQuickSort.cpp
+++ Sorting/HeapQuickSort/HeapQuickSort.cpp
@@ -1,20 +1,24 @@
-// HeapQuickSort.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+void swapArray(int arr[], int x, int y,int size) {
+	int temp = arr[x];
+	arr[x] = arr[y];
+	arr[y] = temp;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void headSort(int arr[], int size) {
+	//1. T?o Max Heap (T?o Tree v?i cha luôn l?n h?n ho?c b?ng 2 con)
+	int indexSon = size - 1;
+	while (indexSon >= 0) {
+		int indexDad = ;
+	}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+}
+
+void Hea
+
+int main() {
+
+}
---
+++ Sorting/MergeSortPriorityQueue/MergeSortPriorityQueue.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+void merge(vector<int> arr) {
+
+}
+
+void mergeSort(vector<int> arr, int left , int right) {
+	if(left<right)
+}
+
+int main() {
+
+}
