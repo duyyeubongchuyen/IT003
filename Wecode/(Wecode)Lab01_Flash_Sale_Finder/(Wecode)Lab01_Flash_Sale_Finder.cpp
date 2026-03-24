41b1a18a5b49d795854b9c523b41eb2071aafd5e
duyyeubongchuyen <nguyenthaiduy0712@gmail.com>
2026-03-24 14:30:51 +0700

    feat: add solution for Flash Sale Finder exercise Assignment 1 Wecode

---
+++ Wecode/(Wecode)Lab01_Flash_Sale_Finder/(Wecode)Lab01_Flash_Sale_Finder.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct PRODUCT {
+	string name;
+	unsigned int price;
+};
+
+vector<string> flashSaleFinder(vector<PRODUCT> products, unsigned int budget) {
+	vector<string> res;
+	for (int i = 0; i < products.size(); i++) {
+		if (products[i].price <= budget) res.push_back(products[i].name);
+	}
+	return res;
+}
+
+void printProducts(vector<string> products) {
+	if (products.empty()) cout << "No items found";
+	else {
+		for (int i = 0; i < products.size(); i++) {
+			cout << products[i] << endl;
+		}
+	}
+}
+
+int main() {
+	unsigned int n;
+	cin >> n;
+	
+	vector<PRODUCT> products(n);
+	for (int i = 0; i < n; i++) {
+		cin >> products[i].name >> products[i].price;
+	}
+
+	unsigned int budget;
+	cin >> budget;
+	printProducts(flashSaleFinder(products, budget));
+	return 0;
+}
