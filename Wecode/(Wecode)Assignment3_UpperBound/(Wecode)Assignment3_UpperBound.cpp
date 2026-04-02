bdad83fe2c2baaf00641b243e3983504f27ca4f7
duyyeubongchuyen <nguyenthaiduy0712@gmail.com>
2026-04-02 17:46:31 +0700

    perf: optimize upper bound(AC)

--- Wecode/(Wecode)Assignment3_UpperBound/(Wecode)Assignment3_UpperBound.cpp
+++ Wecode/(Wecode)Assignment3_UpperBound/(Wecode)Assignment3_UpperBound.cpp
@@ -5,41 +5,66 @@ using namespace std;
 
 typedef long long ll;
 
-int result(vector<int> input,int x) {
-	int i = 0;
+int partition(vector<int>& array, int low, int high) {
+	int pivot = array[high];
 
-	while (!(input[i] > x)) {
-		i++;
-		if (i == input.size()) return -1;
-	}
-	
-	int min = input[i];;
+	int i = low - 1;
 
-	for (i ; i < input.size(); i++) {
-		if ((input[i] < min) && (input[i] > x)) {
-			min = input[i];
+	for (int j = low; j < high; j++) {
+		if (array[j] < pivot) {
+			i++;
+			swap(array[i], array[j]);
 		}
 	}
-	return min;
+	swap(array[high], array[i + 1]);
+	return i + 1;
+}
+
+void quickSort(vector<int>& array,int low,int high) {
+	if (low < high) {
+		int pi = partition(array, low, high);
+		quickSort(array, low, pi - 1);
+		quickSort(array, pi + 1, high);
+	}
+}
+
+int upperBound(const vector<int>& array, int x) {
+	int low = 0;
+	int high = array.size() - 1;
+	int res = -1;
+
+	while (high >= low) {
+		int mid = low + (high - low) / 2;
+		if (array[mid] > x) {
+			res = array[mid];
+			high = mid - 1;
+		}
+		else low = mid + 1;
+	}
+
+	return res;
 }
 
 int main() {
 	int N, Q;
 	cin >> N >> Q;
 
-	vector<int> fir_array(N);
+	vector<int>fir_arr(N);
 	for (int i = 0; i < N; i++) {
-		cin >> fir_array[i];	
-	}
-	
-	vector<int> sec_array(Q);
-	for (int i = 0; i < Q; i++) {
-		cin >> sec_array[i];
+		cin >> fir_arr[i];
 	}
 
+	vector<int>sec_arr(Q);
 	for (int i = 0; i < Q; i++) {
-		cout << result(fir_array, sec_array[i]) << endl;
+		cin >> sec_arr[i];
 	}
+
+	quickSort(fir_arr, 0, N - 1);
+
+	for (int i = 0; i < Q; i++) {
+		cout << upperBound(fir_arr, sec_arr[i]) << "\n";
+	}
+
 
 	return 0;
 }
